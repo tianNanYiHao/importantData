16ed0fc2af818e52be65b30c66a99300593ec6bc
LiuFeiFei <851085835@qq.com>
2016-08-19 15:51:16 +0800

    1212

--- ExcelView-表格控件/LFFExcelVIew/LFFExcel.m
+++ ExcelView-表格控件/LFFExcelVIew/LFFExcel.m
@@ -52,10 +52,14 @@
     UIView *titleVIewBg ; // titleVIewBg
     UIView *cellViewBg ; // 单元格组viewBg
     
+    UIButton *cellBtn;     //单元格按钮
+    
 }
 @synthesize dataSource;
 
--(instancetype)initWithdata:(LFFExcelData*)DataSource{
+-(instancetype)initWithdata:(LFFExcelData*)DataSource block:(LFFExcelComponentBlock)block{
+    _block = block;
+    
     //初始化表格样式 以及获取数据
     dataSource = DataSource;
     //根据titles获取单元格长度
@@ -69,6 +73,7 @@
     self = [super initWithFrame:frame];
     if (self != nil) {
         self.clipsToBounds = YES;
+        self.userInteractionEnabled = YES;
         self.backgroundColor = [UIColor whiteColor];
         self.layer.borderColor = dataSource.lineColor.CGColor;
         self.layer.borderWidth = 1;
@@ -80,15 +85,28 @@
 
 - (void)layoutSubview:(CGRect)frame{
     //添加背景view 以及 Scrollview
+    
+    //标题背景view
     titleVIewBg = [[UIView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, cellHeight)];
     [self addSubview:titleVIewBg];
-    cellViewBg = [[UIView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height-cellHeight)];
+    /*
+     单元格背景view
+     注意这儿的单元格背景view的高度一定要和真正的单元格总共度相同,不能使用自动计算出来的self.frame的高度,
+       那个高度只是整个空间的可视高度,我们实际位单元格们创建的背景view
+    一定是要真正的高度 不然由于高度不够,滚动上来的单元格则不能触发点击效果
+     */
+    cellViewBg = [[UIView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, cellHeight*dataSource.data.count)];
+    
+    //滚动视图
     scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, cellHeight, frame.size.width, frame.size.height-cellHeight)];
     [self addSubview:scrollView];
     [scrollView addSubview:cellViewBg];
+    cellViewBg.userInteractionEnabled = YES;
     scrollView.scrollEnabled = YES;
+    scrollView.userInteractionEnabled = YES;
     scrollView.contentSize = CGSizeMake(frame.size.width, cellHeight*(dataSource.data.count)); //不超出即可
     
+
     
     //1. title视图创建
     for (int i = 0; i<dataSource.titles.count; i++) {
@@ -116,6 +134,7 @@
         vRowView.backgroundColor = [UIColor blueColor];
         vRowView.layer.borderColor = dataSource.lineColor.CGColor;
         vRowView.layer.borderWidth = 0.5;
+        vRowView.userInteractionEnabled = YES;
         [cellViewBg addSubview:vRowView];
         
         //2.2 创建 excel 内容行 中的       *单元格*
@@ -125,6 +144,7 @@
             vCellView.layer.borderWidth = 0.5;
             vCellView.layer.borderColor = dataSource.lineColor.CGColor;
             vCellView.backgroundColor = dataSource.cellColor;
+            vCellView.userInteractionEnabled = YES;
             [vRowView addSubview: vCellView];
             
             //2.2.2为每个单元格创建Lab
@@ -133,9 +153,31 @@
             cellLab.textAlignment = NSTextAlignmentCenter;
             cellLab.textColor = [UIColor grayColor];
             cellLab.text = (dataSource.data[i])[j];
+            
+             //检测到空表格 则停止创建action 按钮
+            if(![[dataSource.data[i] firstObject] isEqualToString:@""]){
+                if (j == dataSource.titles.count-1  && dataSource.anction == YES) {
+                    //2.2.3创建按钮以供点击
+                    cellBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+                    cellBtn.frame = CGRectMake(0, 0, cellWidth, cellHeight);
+                    [cellBtn addTarget:self action:@selector(actionBtn:) forControlEvents:UIControlEventTouchUpInside];
+                    cellLab.enabled = YES;
+                    cellBtn.tag = i;
+                    [vCellView addSubview:cellBtn];
+                    //改变文字颜色
+                    cellLab.textColor = dataSource.titleColor;
+                }
+            }
+            
+            
         }
+        
     }
 
+}
+
+-(void)actionBtn:(UIButton*)btn{
+    _block(btn.tag);
 }
 
 
