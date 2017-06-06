2295b382f4fcd93941ae1f3d31b3af863551118b
liuff <liu.ff_zf@sand.com.cn>
2017-06-06 15:36:57 +0800

    12

---
+++ iOS 代码混淆 :加固/宏定义方法-念茜/codeObfuscation.h
@@ -0,0 +1,48 @@
+#ifndef Demo_codeObfuscation_h
+#define Demo_codeObfuscation_h
+//confuse string at Tue Jun  6 15:17:59 CST 2017
+#define phoneNumTextField eKMhGJEIrEqgqGzn
+#define shortMsgTextField ynRvioKPJOffHDdj
+#define loginPwdTextField XVYAiRhxCGjWEAMj
+#define payPwdTextField iSFFeWwPKfOsUTUO
+#define accPwdTextField BtSnwMkpGgEQziPi
+#define IDCardTextField riRsLLsrGwXKEqGz
+#define pictureNumTextField uumVkiUjfKUlMsYS
+#define bankCardTextField IuVscRLTYZJRlwqW
+#define questionTextField qwzmHeuapNMaJzZm
+#define load sZmvGBHaOXjtNZoD
+#define settingNavigationBar qLtAnswOtiSjmUIb
+#define cusWebView FxrsDYfrbbnsQxFi
+#define cusWebViewDidStartLoad sJSWGjhLhKOVJkZh
+#define buttonActionToDoSomething PLEAVoACHFdIKYCJ
+#define addView osgEOOQNLGHaniUl
+#define SandMajletIndex XuPmPtGpOUEnNFXx
+#define paramString rfMSqqEdEhZuhCCv
+#define payPwd pbOiRDIEIKdOFdxf
+#define defaultPay xcaCIswVmDdnEQjF
+#define TNOrder vqdNCAvoWuToTqdg
+#define initPayMode jhqAZwYNlkWVGVJo
+#define readAuthCode QzbUByvRExOFBTdD
+#define refreshAuthCode dwvdsBmxodbqArRd
+#define oneCodeContentFotmat RRFpCKQhkrXzrxzf
+#define oneCodeTapShowBigView RIplhkrOjypuZoIq
+#define twoCodeImageScaleBigAnimation BYGnhOhwJZOawjZt
+#define buttonAction JsPqdWGKdazjilyz
+#define changePwdShowState ztVFwUDVIFDDcnYx
+#define listenNotifier mbRCFhiiJuZwDlfQ
+#define textFiledEditChanged VCikrWOrfPcXZnpi
+#define textFiledEditChanged JVxyfEnpqvjUqxDw
+#define changeBtnBackGround WVAYyGDwWpximQsy
+#define allInputFields cTCSNQrxFIHMHmGP
+#define textFieldDidBeginEditing bGZmaPhpOxHesAKY
+#define textFieldShouldReturn aJTscOiEpJVJBaZT
+#define setNextResponder yvuUPiJCWrKdpDRC
+#define setInputResponderChain rRrprMVNURVuCRLn
+#define dismissKeyboard TbVrNkaqtzySBRYp
+#define addAuthToolsView AlTkqlDXTRGoqcWt
+#define shortMsgCodeCountDown DnIAuaPZxlqsPdDT
+#define refreshTime eBUIvHyRVcwLjKix
+#define shortMsg JZpGfFjkeSrtMzOq
+#define verificationInput eIEqxRUohKZgPGTq
+#define submit bYTDYzFhVkxjFfIG
+#endif
