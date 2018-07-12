fff6ecc20287e8574df0c446b4b81665fc82acfc
tianNanYiHao <851085835@qq.com>
2018-07-12 10:04:05 +0800

    -a

---
+++ cocoapods-packager封装静态库/开发用demo/pppDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pppDemo
+//
+//  Created by tianNanYiHao on 2018/7/11.
+//  Copyright © 2018年 tianNanYiHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cocoapods-packager封装静态库/测试用demo/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by tianNanYiHao on 2018/7/11.
+//  Copyright © 2018年 tianNanYiHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
